c217fa46dab92b006587a54b5d41957a0ade60fa
Sergio Mavie <sergio.mavie@gmail.com>
2024-03-20 21:03:32 +0200

    Chapter 4: Cleaning up component resources

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+ 
   title = 'learningAngular15';
+
+  ngOnDestroy(): void {
+    console.log('In destroy');
+  }
 }
