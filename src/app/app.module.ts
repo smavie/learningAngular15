33ec23e1890fd4602a97ad7c92a0d4e319e82b26
Sergio Mavie <sergio.mavie@gmail.com>
2024-03-20 18:21:58 +0200

    Chapter 3: products and orders components created and imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductsModule,
+    OrdersModule
   ],
   providers: [
     provideClientHydration()
