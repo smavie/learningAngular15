9c990df5aa9ba4671593eb14e484cdac7be7968d
Sergio Mavie <sergio.mavie@gmail.com>
2024-03-20 19:00:20 +0200

    Chapter 4: Loading the component template

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ProductListComponent
   ]
 })
 export class ProductsModule { }
